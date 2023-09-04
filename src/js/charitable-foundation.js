9a88aae31587c7ab5da063417ff36c4a08d4be3b
YuriiKniaz <yuri.kniaz@gmail.com>
2023-09-04 22:48:43 +0300

    Support Ukraine

--- src/js/charitable-foundation.js
+++ src/js/charitable-foundation.js
@@ -1,10 +1,43 @@
-// const scrollButton = document.querySelector('.scroll-button');
-// const list = document.querySelector('.support-container');
-// const items = document.querySelectorAll('.support-item');
-// const itemHeight = items[0].offsetHeight;
-// let scrollTop = 0;
+import { support } from "./support";
 
-// scrollButton.addEventListener('click', () => {
-//   scrollTop = (scrollTop + itemHeight) % (items.length * itemHeight);
-//   list.scrollTop = scrollTop;
-// });
+const listEl = document.querySelector('.support-list');
+const scrollButton = document.querySelector('.support-next-button');
+const slider = document.querySelector('.supprot-list-container');
+
+let index = 1;
+
+    support.forEach(item => {
+        const listItem = `
+        <div class="support-list-item">
+          <a class="support-list-link" href="${item.url}">
+            0${index++}<img class="support-img" src="${item.img}" alt="${item.title}" height="32"/>
+          </a>
+          </div>
+        `;
+        listEl.insertAdjacentHTML('beforeend', listItem);
+    });
+   
+let scrollAmount = 0;
+const supportItems = document.querySelectorAll('.support-list-item');
+  const itemHeight = supportItems[0].offsetHeight;
+  let scrollTop = 0;
+
+
+
+
+
+  scrollButton.addEventListener('click', () => {
+    scrollTop = (scrollTop + itemHeight) % (supportItems.length * itemHeight);
+    slider.scrollTop = scrollTop;
+  });
+
+    // e.preventDefault();
+    // if (scrollAmount < supportItems.length - 1) {
+    //     scrollAmount++;
+    // } else {
+    //     scrollAmount = 0;
+    // }
+    // const itemHeight = supportItems[0].offsetHeight;
+    // slider.style.transform = `translateY(-${scrollAmount * itemHeight}px)`;
+
+
--- src/js/support.js
+++ src/js/support.js
@@ -1,50 +1,50 @@
-// const support = [
-//   {
-//     title: 'Save the Children',
-//     url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
-//     img: '/src/images/support/save-children.png',
-//   },
-//   {
-//     title: 'Project HOPE',
-//     url: 'https://www.projecthope.org/country/ukraine/',
-//     img: '/src/images/support/hope.png',
-//   },
-//   {
-//     title: 'UNITED24',
-//     url: 'https://u24.gov.ua/uk',
-//     img: '/src/images/support/united24.png',
-//   },
-//   {
-//     title: 'International Medical Corps',
-//     url: 'https://internationalmedicalcorps.org/country/ukraine/',
-//     img: '/src/images/support/medical-corps.png',
-//   },
-//   {
-//     title: 'Medicins Sans Frontieres',
-//     url: 'https://www.msf.org/ukraine',
-//     img: '/src/images/support/sans-frontiers.png',
-//   },
-//   {
-//     title: 'RAZOM',
-//     url: 'https://www.razomforukraine.org/',
-//     img: '/src/images/support/razom.png',
-//   },
-//   {
-//     title: 'Action against hunger',
-//     url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
-//     img: '/src/images/support/against-hunger.png',
-//   },
-//   {
-//     title: 'World vision',
-//     url: 'https://www.wvi.org/emergencies/ukraine',
-//     img: '/src/images/support/world-vision.png',
-//   },
-//   {
-//     title: 'Serhiy Prytula Charity Foundation',
-//     url: 'https://prytulafoundation.org/en',
-//     img: '/src/images/support/prytula.png',
-//   },
-// ];
+export const support = [
+  {
+    title: 'Save the Children',
+    url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
+    img: '/src/images/support/save-children.png',
+  },
+  {
+    title: 'Project HOPE',
+    url: 'https://www.projecthope.org/country/ukraine/',
+    img: '/src/images/support/hope.png',
+  },
+  {
+    title: 'UNITED24',
+    url: 'https://u24.gov.ua/uk',
+    img: '/src/images/support/united24.png',
+  },
+  {
+    title: 'International Medical Corps',
+    url: 'https://internationalmedicalcorps.org/country/ukraine/',
+    img: '/src/images/support/medical-corps.png',
+  },
+  {
+    title: 'Medicins Sans Frontieres',
+    url: 'https://www.msf.org/ukraine',
+    img: '/src/images/support/sans-frontiers.png',
+  },
+  {
+    title: 'RAZOM',
+    url: 'https://www.razomforukraine.org/',
+    img: '/src/images/support/razom.png',
+  },
+  {
+    title: 'Action against hunger',
+    url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
+    img: '/src/images/support/against-hunger.png',
+  },
+  {
+    title: 'World vision',
+    url: 'https://www.wvi.org/emergencies/ukraine',
+    img: '/src/images/support/world-vision.png',
+  },
+  {
+    title: 'Serhiy Prytula Charity Foundation',
+    url: 'https://prytulafoundation.org/en',
+    img: '/src/images/support/prytula.png',
+  },
+];
 // const supportContainer = document.querySelector('.support-container');
 
 // const supportHeaderContainer = document.createElement('div');
