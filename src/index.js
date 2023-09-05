a222cf0c1ace01357f97562028c7ec492e47668f
YuriiKniaz <yuri.kniaz@gmail.com>
2023-09-05 19:04:03 +0300

    Fixed src

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import './js/header-burger.js';
 import './js/theme-switcher.js';
 import './js/book-list.js';
 import './js/best-sellers.js';
-// import './js/shopping-section.js';
+
 import './js/arrow-top.js';
 import './js/registration.js';
 import './js/category-list.js';
 import './js/window-list.js';
 import './js/charitable-foundation.js';
-
 
 //Page change hover
 
