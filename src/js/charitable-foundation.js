9e0a7e9b99b12deff94947613e3547da0a6e1339
Sergey Opasen <ykcyc13@gmail.com>
2023-09-05 10:21:40 +0200

    charitable

--- src/js/charitable-foundation.js
+++ src/js/charitable-foundation.js
@@ -1,43 +1,40 @@
-import { support } from "./support";
+import { support } from './support';
 
 const listEl = document.querySelector('.support-list');
 const scrollButton = document.querySelector('.support-next-button');
-const slider = document.querySelector('.supprot-list-container');
+const slider = document.querySelector('.support-list-container');
 
 let index = 1;
 
-    support.forEach(item => {
-        const listItem = `
-        <div class="support-list-item">
-          <a class="support-list-link" href="${item.url}">
-            0${index++}<img class="support-img" src="${item.img}" alt="${item.title}" height="32"/>
-          </a>
-          </div>
-        `;
-        listEl.insertAdjacentHTML('beforeend', listItem);
-    });
-   
-let scrollAmount = 0;
+support.forEach(item => {
+  const listItem = `
+    <div class="support-list-item">
+      <a class="support-list-link" href="${item.url}">
+        0${index++}<img class="support-img" src="${item.img}" alt="${
+    item.title
+  }" height="32"/>
+      </a>
+    </div>
+    `;
+  listEl.insertAdjacentHTML('beforeend', listItem);
+});
+
 const supportItems = document.querySelectorAll('.support-list-item');
-  const itemHeight = supportItems[0].offsetHeight;
-  let scrollTop = 0;
+const itemHeight = supportItems[0].offsetHeight;
+let currentPosition = 0;
 
+scrollButton.addEventListener('click', () => {
+  currentPosition =
+    (currentPosition + slider.clientHeight) %
+    (supportItems.length * itemHeight);
+  slider.scrollTop = currentPosition;
+});
 
-
-
-
-  scrollButton.addEventListener('click', () => {
-    scrollTop = (scrollTop + itemHeight) % (supportItems.length * itemHeight);
-    slider.scrollTop = scrollTop;
-  });
-
-    // e.preventDefault();
-    // if (scrollAmount < supportItems.length - 1) {
-    //     scrollAmount++;
-    // } else {
-    //     scrollAmount = 0;
-    // }
-    // const itemHeight = supportItems[0].offsetHeight;
-    // slider.style.transform = `translateY(-${scrollAmount * itemHeight}px)`;
-
-
+// e.preventDefault();
+// if (scrollAmount < supportItems.length - 1) {
+//     scrollAmount++;
+// } else {
+//     scrollAmount = 0;
+// }
+// const itemHeight = supportItems[0].offsetHeight;
+// slider.style.transform = `translateY(-${scrollAmount * itemHeight}px)`;
