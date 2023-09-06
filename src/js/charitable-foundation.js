a7d78a9ae4c07c29053a064f52225fcc5bfe1bb9
InnaUsenko <innausenko8@gmail.com>
2023-09-06 17:36:07 +0000

    charitable BugFix

--- src/js/charitable-foundation.js
+++ src/js/charitable-foundation.js
@@ -2,6 +2,7 @@ import { support } from './support';
 
 const listEl = document.getElementById('support-list');
 const scrollButton = document.getElementById('scroll-btn');
+const btnIcon = document.querySelector('.support-btn-icon');
 
 let index = 1;
 let currentItemIndex = 0;
@@ -12,10 +13,12 @@ const loadItems = () => {
   support.forEach(item => {
     const listItem = `
       <li class="support-list-item">
-        <a class="support-list-link" href="${item.url}" target="_blank" aria-label="Link to support Ukraine">
-          0${index++}<img class="support-img" srcset="${item.img} 1x, ${item.img} 2x"src="${item.img}" alt="${
-      item.title
-    }" />
+        <a class="support-list-link" href="${
+          item.url
+        }" target="_blank" aria-label="Link to support Ukraine">
+          0${index++}<img class="support-img" srcset="${item.img} 1x, ${
+      item.img
+    } 2x"src="${item.img}" alt="${item.title}" />
         </a>
       </li>
     `;
@@ -26,7 +29,14 @@ const loadItems = () => {
 };
 
 const scrollToNextItem = () => {
-  currentItemIndex++;
+  let INC = 4;
+  if (window.innerWidth >= 768) {
+    INC = 6;
+  } else {
+    INC = 4;
+  }
+  currentItemIndex = currentItemIndex + INC;
+
   if (currentItemIndex >= maxItems) {
     currentItemIndex = 0;
     listEl.scrollTo({
@@ -34,17 +44,18 @@ const scrollToNextItem = () => {
       behavior: 'auto',
     });
   } else {
-    
     listEl.scrollTo({
       top: currentItemIndex * listItemHeight,
       behavior: 'smooth',
     });
+  }
+  if (currentItemIndex + INC >= maxItems) {
+    btnIcon.classList.add('rotate');
+  } else {
+    btnIcon.classList.remove('rotate');
   }
 };
 
 scrollButton.addEventListener('click', scrollToNextItem);
 
 loadItems();
-
-
-
